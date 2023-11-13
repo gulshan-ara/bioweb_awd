0ef37e3234f812b3617a7d882be22b21d3f72e37
gulshan-ara <gulshan3015@gmail.com>
2023-11-13 16:09:13 +0600

    build - db tables with column names are added as models

--- genedata/models.py
+++ genedata/models.py
@@ -1,5 +1,48 @@
 from django.db import models
 
-#  demo model with only a field named text
-class Hello(models.Model):
-  text = models.CharField(max_length=200)
+# db tables with columns & datatypes
+class EC(models.Model):
+  ec_name = models.CharField(max_length=256, null=False, blank=False)
+
+  def __str__(self):
+    return self.ec_name
+
+class Sequencing(models.Model):
+  sequencing_factory = models.CharField(max_length=256, null=False, blank=False)
+  factory_location = models.CharField(max_length=256, null=False, blank=False)
+
+  def __str__(self):
+    return self.sequencing_factory
+
+class Gene(models.Model):
+  gene_id = models.CharField(max_length=256, null=False, blank=False)
+  entity = models.CharField(max_length=256, null=False, blank=False)
+  start = models.IntegerField(null=False, blank=True)
+  stop = models.IntegerField(null=False, blank=True)
+  sense = models.CharField(max_length=1)
+  start_codon = models.CharField(max_length=1, default='M')
+  sequencing = models.ForeignKey(Sequencing, on_delete=models.DO_NOTHING)
+  ec = models.ForeignKey(EC, on_delete=models.DO_NOTHING)
+
+  def __str__(self):
+    return self.gene_id
+
+class Product(models.Model):
+  type = models.CharField(max_length=256, null=False, blank=False)
+  product = models.CharField(max_length=256, null=False, blank=False)
+  gene = models.ForeignKey(Gene, on_delete=models.DO_NOTHING)
+
+  def __str__(self):
+    return self.product
+
+class Attribute(models.Model):
+  key = models.CharField(max_length=256, null=False, blank=False)
+  value = models.CharField(max_length=256, null=False, blank=False)
+  gene = models.ManyToManyField(Gene, through="GeneAttributeLink")
+
+  def __str__(self):
+    return self.key + " : " + self.value
+
+class GeneAttributeLink(models.Model):
+  gene = models.ForeignKey(Gene, on_delete=models.DO_NOTHING)
+  attribute = models.ForeignKey(Attribute, on_delete=models.DO_NOTHING)
--- genedata/views.py
+++ genedata/views.py
@@ -3,5 +3,6 @@ from .models import *
 
 # Index function promised in urls
 def index(request):
-  response_string = Hello.objects.all()
+  # fetching values from Gene table
+  response_string = Gene.objects.all()[0]
   return render(request, 'genedata/index.html', { 'data' : response_string})
