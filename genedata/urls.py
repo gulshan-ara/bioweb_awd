82ca45177925e1270e2cf818057541081f4f311f
gulshan-ara <gulshan3015@gmail.com>
2023-11-13 12:16:27 +0600

    build - the database is migrated to django project

--- genedata/urls.py
+++ genedata/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlPatterns = [
+urlpatterns = [
   # the request for root path serves the index function in views file
   path('', views.index, name='index'),
 ]
