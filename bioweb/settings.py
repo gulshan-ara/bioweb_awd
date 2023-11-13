36f1c9a1799c1cb971e1ee086664303288dbe7aa
gulshan-ara <gulshan3015@gmail.com>
2023-11-13 12:10:47 +0600

    build - app is inserted in the settings file to bind with django project

--- bioweb/settings.py
+++ bioweb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'genedata.apps.GenedataConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
