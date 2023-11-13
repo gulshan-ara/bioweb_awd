b1a66671a48e9c2ecef3d597f96a6b5527f0b178
gulshan-ara <gulshan3015@gmail.com>
2023-11-13 16:32:18 +0600

    build - models are registered to show in admin site

--- genedata/admin.py
+++ genedata/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
+
+class GeneAttributeLinkInline(admin.TabularInline): 
+  model = GeneAttributeLink
+  extra = 3
 
 # Register your models here.
+class GeneAdmin(admin.ModelAdmin):
+  list_display = ("gene_id", "entity", "start", "stop", "sense")
+  inlines = [GeneAttributeLinkInline]
+
+class ECAdmin(admin.ModelAdmin):
+  list_display = ("ec_name",)
+
+class SequencingAdmin(admin.ModelAdmin):
+  list_display = ("sequencing_factory", "factory_location")
+
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ("type", "product")
+
+class AttributeAdmin(admin.ModelAdmin):
+  list_display = ("key", "value")
+
+
+admin.site.register(Gene, GeneAdmin)
+admin.site.register(EC, ECAdmin)
+admin.site.register(Sequencing, SequencingAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Attribute, AttributeAdmin)
